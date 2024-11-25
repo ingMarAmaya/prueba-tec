83f43cce6f9b098dda5733f37f7e4291a9eb10e5
Marce Amaya <ing.orellana3@gmail.com>
2024-11-25 11:22:22 -0600

    added style to tarjetas

--- prueba/src/components/tarjetas.vue
+++ prueba/src/components/tarjetas.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
     <div>
-        <section>
+        <section class="tarjetas">
             <article class="tarjeta">
                 <h2>Producto: </h2>
                 <p>Precio: </p>
@@ -13,5 +13,24 @@
     </div>
 </template>
 <style scoped>
-
+    .tarjetas{
+        margin: 0px auto;
+        width: 70%;
+    }
+    .tarjeta{
+        display: block;
+        float: left;
+        margin: 0px auto;
+        width: 25%;
+        height: 200px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin: 5px;
+        padding: 15px;
+    }
+    .tarjeta h2{
+        color: white;
+        text-align: center;
+        padding: 10px;
+    }
 </style>
